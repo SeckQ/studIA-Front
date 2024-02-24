f01931670bfb0b1ac44962f61c157bdf1b4e19db
Francisco <edjara92@gmail.com>
2024-02-24 14:46:08 -0500

    Reparar2

---
+++ HomeScreen.js
@@ -0,0 +1,17 @@
+import {Button} from "react-native";
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <Button
+            title="Go to Jane's profile"
+            onPress={() =>
+                navigation.navigate('Profile', {name: 'Jane'})
+            }
+        />
+    );
+};
+const ProfileScreen = ({navigation, route}) => {
+    return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+export default HomeScreen
