1df175b93a7e4c0fffa106ee22d861f12845c100
Steven <stevenortizpro@hotmail.com>
2024-02-28 18:58:57 -0500

    mi ultima chamba

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,40 +1,62 @@
 import * as React from "react";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {NavigationContainer, DefaultTheme} from "@react-navigation/native";
-import {useFonts} from "expo-font";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 import Carga from "./screens/Carga";
 import InicioSesion from "./screens/InicioSesion";
 import Inicio from "./screens/Inicio";
 import Nota from "./screens/Nota";
-import FAIcon from "react-native-vector-icons/FontAwesome5";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Notificaciones from "./screens/Notificaiones";
 import Perfil from "./screens/Perfil";
 import TusMaterias from "./screens/TusMaterias";
 import PerfilMateria from "./screens/PerfilMateria";
 import ListaNotas from "./screens/ListaNotas";
-import Repaso from "./screens/Repaso"
+import Repaso from "./screens/Repaso";
 import DetallesNota from "./screens/DetallesNota";
-
-
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Stack = createNativeStackNavigator();
-const Tabs = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-
+const MainTabs = () => {
+    return (
+        <Tab.Navigator initialRouteName="Inicio">
+            <Tab.Screen
+                name={'Inicio'}
+                component={Inicio}
+                options={{
+                    tabBarLabel: 'Inicio',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name={'home'} color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name={'Notificaciones'}
+                component={Notificaciones}
+                options={{
+                    tabBarLabel: 'Notificaciones',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name={'bell-ring'} color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name={'Perfil'}
+                component={Perfil}
+                options={{
+                    tabBarLabel: 'Perfil',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name={'account'} color={color} size={size} />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
 
 const Navigation = () => {
     const [loaded, setLoaded] = React.useState(false);
-
-    let [fontsLoaded] = useFonts({
-        'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
-        'Epilogue-Bold': require('../assets/fonts/Epilogue-Bold.ttf'),
-        'Inter-Bold': require('../assets/fonts/Inter-Bold.ttf'),
-        'Inter-Medium': require('../assets/fonts/Inter-Medium.ttf'),
-        'Inter-Light': require('../assets/fonts/Inter-Light.ttf'),
-        'Epilogue-SemiBold': require('../assets/fonts/Epilogue-SemiBold.ttf'),
-        'Epilogue-Regular': require('../assets/fonts/Epilogue-Regular.ttf'),
-    });
 
     React.useEffect(() => {
         setTimeout(() => {
@@ -42,67 +64,63 @@ const Navigation = () => {
         }, 1000);
     }, []);
 
-    if (!fontsLoaded) {
-        return null;
-    }
-
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Navigator screenOptions={{ headerShown: false }} headerMode="none">
                 {!loaded ? (
                     <Stack.Screen
                         name="Carga"
                         component={Carga}
-                        options={{headerShown: false}}
+                        options={{ headerShown: false }}
                     />
                 ) : (
                     <>
                         <Stack.Screen
+                            name="MainTabs"
+                            component={MainTabs}
+                            options={{ headerShown: false }}
+                        />
+
+                        <Stack.Screen
                             name="DetallesNotas"
                             component={DetallesNota}
-                            options={{headerShown: false}}
+                            options={{ headerShown: false }}
                         />
 
                         <Stack.Screen
                             name="ListaNotas"
                             component={ListaNotas}
-                            options={{headerShown: false}}
+                            options={{ headerShown: false }}
                         />
 
                         <Stack.Screen
                             name="Repaso"
                             component={Repaso}
-                            options={{headerShown: true}}
+                            options={{ headerShown: true }}
                         />
 
                         <Stack.Screen
                             name="PerfilMateria"
                             component={PerfilMateria}
-                            options={{headerShown: true}}
+                            options={{ headerShown: true }}
                         />
 
-                        <Stack.Screen
-                            name="Tabs"
-                            component={TabsNav}
-                            options={{ headerShown: false }}
-                        />
-
-                        <Stack.Screen
-                            name="TusMaterias"
-                            label="Tus Materias"
-                            component={TusMaterias}
-                            options={{headerShown: true}}
-                        />
                         <Stack.Screen
                             name="Nota"
                             component={Nota}
-                            options={{headerShown: false}}
+                            options={{ headerShown: false }}
                         />
 
                         <Stack.Screen
                             name="InicioSesion"
                             component={InicioSesion}
-                            options={{headerShown: false}}
+                            options={{ headerShown: false }}
+                        />
+
+                        <Stack.Screen
+                            name="TusMaterias"
+                            component={TusMaterias}
+                            options={{ headerShown: true }}
                         />
                     </>
                 )}
@@ -111,36 +129,5 @@ const Navigation = () => {
     );
 };
 
-function TabsNav() {
-    return (
-        <Tabs.Navigator
-            screenOptions={({ route }) => {
-                return {
-                    headerShown: false,
-                    tabBarIcon: ({ focused, size }) => {
-                        let icon = "";
-                        switch (route.name) {
-                            case "Inicio":
-                                icon = "home";
-                                break;
-                            case "Notificaciones":
-                                icon="bell";
-                                break;
-                            case "Perfil":
-                                icon = "user";
-                                break;
-                        }
-
-                        return <FAIcon name={icon} size={size} color="#000000" />;
-                    },
-                };
-            }}
-        >
-            <Tabs.Screen name="Inicio" component={Inicio} />
-            <Tabs.Screen name="Notificaciones" component={Notificaciones} />
-            <Tabs.Screen name="Perfil" component={Perfil} />
-        </Tabs.Navigator>
-    );
-}
-
 export default Navigation;
+
